03e5b1007af779e9e46f218a95bd764396570aa9
Kess220 <prodvictorkaio@gmail.com>
2024-02-19 08:54:06 -0300

    Fixed HTTP status responses for error handling.

--- src/main/java/com/boardcamp/api/errors/ExistingCpfExceptionError.java
+++ src/main/java/com/boardcamp/api/errors/ExistingCpfExceptionError.java
@@ -2,6 +2,6 @@ package com.boardcamp.api.errors;
 
 public class ExistingCpfExceptionError extends RuntimeException {
     public ExistingCpfExceptionError(String cpf) {
-        super("Já existe um cliente registrado com o CPF: " + cpf);
+        super("There is already a registered customer with CPF: " + cpf);
     }
 }
